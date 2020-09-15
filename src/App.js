bcda35c07f8f461c7a2ef7c6bceef07a307b800e
gitnubl33t <jessicad8695@gmail.com>
2020-09-15 13:48:29 -0700

    added nav bar and h1 text.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <nav></nav>
+      <header className="app-header">
+        <nav>
+          <a href="">Home</a>
+          <a href="">Contact</a>
+          <a href="">About</a>
+        </nav>
         <h1>Calories on the Go</h1>
       </header>
     </div>
